c28a26aa4ba0029c815ae6950f93a4e6eedbfece
erol <je.evangelista04@gmail.com>
2022-06-06 21:23:45 -0400

    Added ingest scripts

---
+++ scripts/ingestion/populate.py
@@ -0,0 +1,85 @@
+import sys
+import os
+import json
+import glob
+from py2neo import Graph, Node, Relationship
+from tqdm import tqdm
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class GraphEx:
+  ''' A convenient wrapper around Graph which regularly
+  commits intermediately (this seems to improve performance)
+  '''
+  chunk_size = 2000
+
+  def __init__(self, *args, **kwargs):
+    self.graph = Graph(*args, **kwargs)
+    self.graph.delete_all()
+    self.n = 0
+    self.tx = None
+
+  def _begin(self):
+    if self.tx is None:
+      self.n = 0
+      self.tx = self.graph.begin()
+
+  def commit(self):
+    if self.tx is not None:
+      self.graph.commit(self.tx)
+      self.tx = None
+
+  def _periodic_commit(self):
+    self.n += 1
+    if self.n % GraphEx.chunk_size == 0:
+      self.commit()
+
+  def create(self, obj):
+    self._begin()
+    self.tx.create(obj)
+    self._periodic_commit()
+  
+  def merge(self, obj):
+    self._begin()
+    if obj["id"]:
+      self.tx.merge(obj, primary_label="id", primary_key="id")
+    else:
+      self.tx.merge(obj, primary_label="symbol", primary_key="symbol")
+    self._periodic_commit()
+  
+  def run(self, parameters=None, **kwparameters):
+    return self.graph.run(parameters=parameters, **kwparameters)
+
+
+neo4graph = GraphEx(os.environ['NEO4J_URL'], auth=(os.environ['NEO4J_USER'], os.environ['NEO4J_PASSWORD']))
+
+
+# python populate.py /path/to/files/to/ingest
+directory = sys.argv[1]
+
+for filename in glob.glob(directory + "/*.valid.json"):
+  with open(filename) as o:
+    print("Ingesting %s"%filename)
+    serialized = json.loads(o.read())
+    for i in tqdm(serialized["edges"]):
+      source = i["source"]
+      node_a_props = serialized["nodes"][source]
+      node_a_properties = node_a_props.get("properties", {})
+      node_a_type = node_a_props["type"]
+      node_a = Node(node_a_type, **node_a_properties)
+      neo4graph.merge(node_a)
+
+      target = i["target"]
+      node_b_props = serialized["nodes"][target]
+      node_b_properties = node_b_props.get("properties", {})
+      node_b_type = node_b_props["type"]
+      node_b = Node(node_b_type, **node_b_properties)
+      neo4graph.merge(node_b)
+
+      relation = i["relation"]
+      relation_properties_dict = i.get("properties", {})
+      neo4graph.merge(Relationship(node_a, relation, node_b, **relation_properties_dict))
+
+neo4graph.commit()
---
+++ scripts/ingestion/validation.py
@@ -0,0 +1,72 @@
+import sys
+import json
+import glob
+
+# python validation /path/to/v2/files
+
+type_mapper = {
+    "int": int,
+    "number": float,
+    "string": str,
+    "object": dict,
+    "array": list
+}
+
+def validation(entry):
+    if '@type' not in entry:
+        raise Exception("@type not found in %s"%json.dumps(entry))
+    if '@value' not in entry:
+        raise Exception("@value not found in %s"%json.dumps(entry))
+    valid = isinstance(entry["@value"], type_mapper[entry["@type"]])
+    if not valid:
+        raise Exception("Invalid @type %s: %s"%(entry["@type"],entry["@value"]))
+    elif entry["@type"] == "array" and len(entry["@value"]) > 0:
+        valid_array = []
+        for i in entry["@value"]:
+            valid_array.append(validation(i))
+        return(valid_array)
+    elif entry["@type"] == "object" and len(entry["@value"]) > 0:
+        valid_object = {}
+        for k,v in entry["@value"].items():
+            valid_object[k] = validation(v)
+        return valid_object
+    
+    return entry["@value"]
+
+
+def validate(unvalidated_json):
+    try:
+        if not unvalidated_json["version"] == '2':
+            raise Exception("Please enter KG Serialization version 2")
+        validated_json = {
+            "version": "1"
+        }
+
+        nodes = {}
+        for k,v in unvalidated_json["nodes"].items():
+            n = validation(v)
+            nodes[k] = n
+        validated_json["nodes"] = nodes
+        edges = []
+        for i in unvalidated_json["edges"]:
+            e = validation(i)
+            if not e["source"] in nodes:
+                raise Exception("Cannot find source %s on nodes"%e["source"])
+            if not e["target"] in nodes:
+                raise Exception("Cannot find target %s on nodes"%e["target"])
+            edges.append(e)
+        validated_json["edges"] = edges  
+        return validated_json
+    except Exception as e:
+        print(e)
+        return False
+
+directory = sys.argv[1]
+
+for filename in glob.glob(directory + "/*.v2.json"):
+	with open(filename) as o:
+		unvalidated_json = json.loads(o.read())
+	if not unvalidated_json == False:
+		print("Validated %s"%filename)
+		with open(filename.replace(".v2.json", ".valid.json"), 'w') as o:
+			o.write(json.dumps(validate(unvalidated_json)))
