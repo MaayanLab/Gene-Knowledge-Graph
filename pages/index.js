b91b25338c429ea3a727ee21fa163c7834a868de
erol <je.evangelista04@gmail.com>
2022-06-29 13:04:25 -0400

    fixed double query issue

--- pages/index.js
+++ pages/index.js
@@ -24,6 +24,7 @@ const ToggleButtonGroup = dynamic(() => import('@mui/material/ToggleButtonGroup'
 const Switch = dynamic(() => import('@mui/material/Switch'));
 const Card = dynamic(() => import('@mui/material/Card'));
 const CardContent = dynamic(() => import('@mui/material/CardContent'));
+const CircularProgress = dynamic(() => import('@mui/material/CircularProgress'));
 const Slider = dynamic(() => import('@mui/material/Slider'));
 const Cytoscape = dynamic(() => import('../components/Cytoscape'), { ssr: false })
 const CameraAltOutlinedIcon  = dynamic(() => import('@mui/icons-material/CameraAltOutlined'));
@@ -285,7 +286,6 @@ export default function KnowledgeGraph({entries, nodes, examples=default_example
               <Grid item xs={12}>
                 <Typography variant="body1">End with</Typography>
                 <Selector entries={entries} value={end} prefix={"End"} onChange={(e)=>{
-                  console.log(e)
                   redirect({start, start_term, end: e, limit})
                 }}/>
               </Grid>
@@ -406,6 +406,8 @@ function KnowledgeGraphViz(props) {
   const [edgeStyle, setEdgeStyle] = React.useState({label: 'data(label)'})
   const [controller, setController] = React.useState(null)
   const [layout, setLayout] = React.useState(Object.keys(layouts)[0])
+  const [loading, setLoading] = React.useState(false)
+
   const cyref = useRef(null);
   
   const get_controller = () => {
@@ -417,7 +419,7 @@ function KnowledgeGraphViz(props) {
   
   const resolve_elements = async (isActive) => {
     try {
-      setElements(undefined)
+      setLoading(true)
       const controller = get_controller()
       const body = {
         start,
@@ -439,6 +441,7 @@ function KnowledgeGraphViz(props) {
       const results = await res.json()
       if (!isActive) return
       setElements(results)
+      setLoading(false)
       setData(results)
       setId(id => id+1)
     } catch (error) {
@@ -460,23 +463,25 @@ function KnowledgeGraphViz(props) {
     } else {
       await  resolve_elements(isActive)
     }
-  }, [start_term])
+  }, [start_term, end_term, limit])
 
-  useAsyncEffect(async (isActive) => {
-    if (start_term && end_term) {
-      await  resolve_elements(isActive)
-    }
-  }, [end_term])
+  // useAsyncEffect(async (isActive) => {
+  //   if (start_term && end_term) {
+  //     console.log("HO")
+  //     await  resolve_elements(isActive)
+  //   }
+  // }, [end_term])
 
-  useAsyncEffect(async (isActive) => {
-    if (start_term) {
-      await  resolve_elements(isActive)
-    }
-  }, [limit])
+  // useAsyncEffect(async (isActive) => {
+  //   if (start_term) {
+  //     console.log("HID")
+  //     await  resolve_elements(isActive)
+  //   }
+  // }, [limit])
   return (
     <Box>
       {(elements === undefined) ? (
-        <div>Loading...</div>
+        <CircularProgress/>
       ) : elements.length === 0 ? (
         <div>No results</div>
       ) : (
@@ -570,169 +575,171 @@ function KnowledgeGraphViz(props) {
             </Button>
           </Grid>
           <Grid item xs={12}>
-            <Cytoscape
-              key={id}
-              wheelSensitivity={0.1}
-              style={{
-                width: '100%',
-                height: 700,
+            {loading ? <CircularProgress/>:
+              <Cytoscape
+                key={id}
+                wheelSensitivity={0.1}
+                style={{
+                  width: '100%',
+                  height: 700,
+                }}
+                stylesheet={[
+                  {
+                    selector: 'node',
+                    style: {
+                      'background-color': 'data(color)',
+                      'label': 'data(label)',
+                      "text-valign": "center",
+                      "text-halign": "center",
+                      'width': `mapData(node_type, 0, 1, 70, 150)`,
+                      'height': `mapData(node_type, 0, 1, 70, 150)`,
+                    }
+                  },
+                  {
+                    selector: 'edge',
+                    style: {
+                      'curve-style': 'straight',
+                      // 'opacity': '0.5',
+                      'line-color': 'data(lineColor)',
+                      'width': '3',
+                      // 'label': 'data(label)',
+                      "text-rotation": "autorotate",
+                      "text-margin-x": "0px",
+                      "text-margin-y": "0px",
+                      'font-size': '12px',
+                      'target-arrow-shape': `data(directed)`,
+                      'target-endpoint': 'outside-to-node',
+                      'source-endpoint': 'outside-to-node',
+                      'target-arrow-color': 'data(lineColor)',
+                      ...edgeStyle
+                    }
+                  },
+                  {
+                    selector: 'node.highlight',
+                    style: {
+                        'border-color': 'gray',
+                        'border-width': '2px',
+                        'font-weight': 'bold',
+                        'font-size': '18px',
+                        'width': `mapData(node_type, 0, 1, 90, 170)`,
+                        'height': `mapData(node_type, 0, 1, 90, 170)`,
+                    }
+                  },
+                  {
+                    selector: 'node.focused',
+                    style: {
+                        'border-color': 'gray',
+                        'border-width': '2px',
+                        'font-weight': 'bold',
+                        'font-size': '18px',
+                        'width': `mapData(node_type, 0, 1, 90, 170)`,
+                        'height': `mapData(node_type, 0, 1, 90, 170)`,
+                    }
+                  },
+                  {
+                    selector: 'edge.focusedColored',
+                    style: {
+                        'line-color': '#F8333C',
+                        'width': '6'
+                    }
+                  },
+                  {
+                    selector: 'node.semitransp',
+                    style:{ 'opacity': '0.5' }
+                  },
+                  {
+                    selector: 'node.focusedSemitransp',
+                    style:{ 'opacity': '0.5' }
+                  },
+                  {
+                    selector: 'edge.colored',
+                    style: {
+                        'line-color': '#F8333C',
+                        'target-arrow-color': '#F8333C',
+                        'width': '6'
+                    }
+                  },
+                  {
+                    selector: 'edge.semitransp',
+                    style:{ 'opacity': '0.5' }
+                  },
+                  {
+                    selector: 'edge.focusedSemitransp',
+                    style:{ 'opacity': '0.5' }
+                  }
+                ]}
+                elements={elements}
+                layout={layouts[layout]}
+                cy={(cy) => {
+                  cyref.current = cy
+                  cy.on('click', 'node', function (evt) {
+                  // setAnchorEl(null)
+                  const node = evt.target.data()
+
+                  if (focused && node.id === focused.id) {
+                    const sel = evt.target;
+                    cy.elements().removeClass('focusedSemitransp');
+                    sel.removeClass('focused').outgoers().removeClass('focusedColored')
+                    sel.incomers().removeClass('focusedColored')
+                    setNode({node: null, type: "focused"})
+                  } else{
+                    const sel = evt.target;
+                    cy.elements().removeClass('focused');
+                    cy.elements().removeClass('focusedSemitransp');
+                    cy.elements().removeClass('focusedColored');
+                    cy.elements().not(sel).addClass('focusedSemitransp');
+                    sel.addClass('focused').outgoers().addClass('focusedColored')
+                    sel.incomers().addClass('focusedColored')
+                    sel.incomers().removeClass('focusedSemitransp')
+                    sel.outgoers().removeClass('focusedSemitransp')
+                    setNode({node, type: "focused"})
+                  }
+                })
+
+                cy.nodes().on('mouseover', (evt) => {
+                  const n = evt.target.data()
+                  const sel = evt.target;
+                  cy.elements().not(sel).addClass('semitransp');
+                  sel.addClass('highlight').outgoers().addClass('colored')
+                  sel.incomers().addClass('colored')
+                  sel.incomers().removeClass('semitransp')
+                  sel.outgoers().removeClass('semitransp')
+                  if (n.id !== (node || {}).id) {
+                    // setAnchorEl(evt.target.popperRef())
+                    setNode({node: n})
+                  }
+                });
+
+                cy.nodes().on('mouseout', (evt) => {
+                  const sel = evt.target;
+                  cy.elements().removeClass('semitransp');
+                  sel.removeClass('highlight').outgoers().removeClass('colored')
+                  sel.incomers().removeClass('colored')
+                  // setAnchorEl(null)
+                  setNode({node: null})
+                
+                });
+                cy.edges().on('mouseover', (evt) => {
+                  const n = evt.target.data()
+                  const sel = evt.target;
+                  cy.elements().not(sel).addClass('semitransp');
+                  sel.addClass('colored').connectedNodes().addClass('highlight')
+                  sel.connectedNodes().removeClass('semitransp')
+                  if (n.id !== (node || {}).id) {
+                    // setAnchorEl(evt.target.popperRef())
+                    setNode({node: n})
+                  }
+                });
+                cy.edges().on('mouseout', (evt) => {
+                  const sel = evt.target;
+                  cy.elements().removeClass('semitransp');
+                  sel.removeClass('colored').connectedNodes().removeClass('highlight')
+                  // setAnchorEl(null)
+                  setNode({node: null})
+                });
               }}
-              stylesheet={[
-                {
-                  selector: 'node',
-                  style: {
-                    'background-color': 'data(color)',
-                    'label': 'data(label)',
-                    "text-valign": "center",
-                    "text-halign": "center",
-                    'width': `mapData(node_type, 0, 1, 70, 150)`,
-                    'height': `mapData(node_type, 0, 1, 70, 150)`,
-                  }
-                },
-                {
-                  selector: 'edge',
-                  style: {
-                    'curve-style': 'straight',
-                    // 'opacity': '0.5',
-                    'line-color': 'data(lineColor)',
-                    'width': '3',
-                    // 'label': 'data(label)',
-                    "text-rotation": "autorotate",
-                    "text-margin-x": "0px",
-                    "text-margin-y": "0px",
-                    'font-size': '12px',
-                    'target-arrow-shape': `data(directed)`,
-                    'target-endpoint': 'outside-to-node',
-                    'source-endpoint': 'outside-to-node',
-                    'target-arrow-color': 'data(lineColor)',
-                    ...edgeStyle
-                  }
-                },
-                {
-                  selector: 'node.highlight',
-                  style: {
-                      'border-color': 'gray',
-                      'border-width': '2px',
-                      'font-weight': 'bold',
-                      'font-size': '18px',
-                      'width': `mapData(node_type, 0, 1, 90, 170)`,
-                      'height': `mapData(node_type, 0, 1, 90, 170)`,
-                  }
-                },
-                {
-                  selector: 'node.focused',
-                  style: {
-                      'border-color': 'gray',
-                      'border-width': '2px',
-                      'font-weight': 'bold',
-                      'font-size': '18px',
-                      'width': `mapData(node_type, 0, 1, 90, 170)`,
-                      'height': `mapData(node_type, 0, 1, 90, 170)`,
-                  }
-                },
-                {
-                  selector: 'edge.focusedColored',
-                  style: {
-                      'line-color': '#F8333C',
-                      'width': '6'
-                  }
-                },
-                {
-                  selector: 'node.semitransp',
-                  style:{ 'opacity': '0.5' }
-                },
-                {
-                  selector: 'node.focusedSemitransp',
-                  style:{ 'opacity': '0.5' }
-                },
-                {
-                  selector: 'edge.colored',
-                  style: {
-                      'line-color': '#F8333C',
-                      'target-arrow-color': '#F8333C',
-                      'width': '6'
-                  }
-                },
-                {
-                  selector: 'edge.semitransp',
-                  style:{ 'opacity': '0.5' }
-                },
-                {
-                  selector: 'edge.focusedSemitransp',
-                  style:{ 'opacity': '0.5' }
-                }
-              ]}
-              elements={elements}
-              layout={layouts[layout]}
-              cy={(cy) => {
-                cyref.current = cy
-                cy.on('click', 'node', function (evt) {
-                // setAnchorEl(null)
-                const node = evt.target.data()
-
-                if (focused && node.id === focused.id) {
-                  const sel = evt.target;
-                  cy.elements().removeClass('focusedSemitransp');
-                  sel.removeClass('focused').outgoers().removeClass('focusedColored')
-                  sel.incomers().removeClass('focusedColored')
-                  setNode({node: null, type: "focused"})
-                } else{
-                  const sel = evt.target;
-                  cy.elements().removeClass('focused');
-                  cy.elements().removeClass('focusedSemitransp');
-                  cy.elements().removeClass('focusedColored');
-                  cy.elements().not(sel).addClass('focusedSemitransp');
-                  sel.addClass('focused').outgoers().addClass('focusedColored')
-                  sel.incomers().addClass('focusedColored')
-                  sel.incomers().removeClass('focusedSemitransp')
-                  sel.outgoers().removeClass('focusedSemitransp')
-                  setNode({node, type: "focused"})
-                }
-              })
-
-              cy.nodes().on('mouseover', (evt) => {
-                const n = evt.target.data()
-                const sel = evt.target;
-                cy.elements().not(sel).addClass('semitransp');
-                sel.addClass('highlight').outgoers().addClass('colored')
-                sel.incomers().addClass('colored')
-                sel.incomers().removeClass('semitransp')
-                sel.outgoers().removeClass('semitransp')
-                if (n.id !== (node || {}).id) {
-                  // setAnchorEl(evt.target.popperRef())
-                  setNode({node: n})
-                }
-              });
-
-              cy.nodes().on('mouseout', (evt) => {
-                const sel = evt.target;
-                cy.elements().removeClass('semitransp');
-                sel.removeClass('highlight').outgoers().removeClass('colored')
-                sel.incomers().removeClass('colored')
-                // setAnchorEl(null)
-                setNode({node: null})
-              
-              });
-              cy.edges().on('mouseover', (evt) => {
-                const n = evt.target.data()
-                const sel = evt.target;
-                cy.elements().not(sel).addClass('semitransp');
-                sel.addClass('colored').connectedNodes().addClass('highlight')
-                sel.connectedNodes().removeClass('semitransp')
-                if (n.id !== (node || {}).id) {
-                  // setAnchorEl(evt.target.popperRef())
-                  setNode({node: n})
-                }
-              });
-              cy.edges().on('mouseout', (evt) => {
-                const sel = evt.target;
-                cy.elements().removeClass('semitransp');
-                sel.removeClass('colored').connectedNodes().removeClass('highlight')
-                // setAnchorEl(null)
-                setNode({node: null})
-              });
-            }}
-            />
+              />
+            }
           </Grid>        
         </Grid>
       )}
