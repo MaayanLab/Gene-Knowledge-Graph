c80639b36b7060a7bd8c1db809eb787906508406
John Erol Evangelista <je.evangelista04@gmail.com>
2022-07-06 10:45:51 -0400

    initialize info

--- utils/initialize.js
+++ utils/initialize.js
@@ -40,6 +40,7 @@ export async function get_terms(node) {
 					return {...acc, [g.properties.label]: g.properties}
 				}, entries)
 			skip = skip + limit	
+			console.log(`Done with ${skip}/${count}`)
 		}
 		return entries
 	}
