e090755496e1467f300998ece39ba3e1f7ddd56b
erol <je.evangelista04@gmail.com>
2022-07-07 20:22:54 -0400

    added buttons to header

--- components/header.js
+++ components/header.js
@@ -1,21 +1,84 @@
+import { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 import Image from 'next/image'
 import { makeTemplate } from "../utils/helper";
 import * as default_schema from '../public/schema.json'
+import { useRouter } from 'next/router'
 
 const Grid = dynamic(() => import('@mui/material/Grid'));
 const Typography = dynamic(() => import('@mui/material/Typography'));
+const Button = dynamic(() => import('@mui/material/Button'));
 
-const Header = ({schema, ...rest}) => {
-	if (!schema) schema = default_schema
-	if (schema === undefined || schema.header === undefined) return null
-	return(
-	<Grid container justifyContent={"center"} alignItems={"center"} spacing={2} style={{marginBottom: 20}}>
-		<Grid item xs={12} align="center">
-			<Typography variant="h4">{schema.header.title}</Typography>
-		</Grid>
-		{((schema.header || {}).subheader||[]).map(({label, icon, height=100, width=100})=>(
-			<Grid item key={label} style={{marginLeft: 10, marginRight: 10}}>
+const function_mapper = {
+	filter_relation: ({relations, router, selected, label, relation})=>{
+		if (relations.join(",") === relation) {
+			const {relation: re, ...query} = router.query
+			router.push({
+				pathname: router.route || '/',
+				query
+			  }, undefined, {shallow: true})
+		}
+		else {
+			router.push({
+				pathname: router.route || '/',
+				query: {
+					...router.query,
+					relation: relations.join(",")
+				}
+			  }, undefined, {shallow: true})
+		}
+	}
+}
+
+const styles = {
+	disabled: {
+		opacity: .4
+	},
+	enabled: {
+		opacity: 1,
+	},
+	active: {
+		opacity: 1,
+		background: "#e0e0e0",
+		"&:hover": {
+			background: "#9e9e9e",
+		}
+	}
+  }
+
+const IconRenderer = ({label, icon, height=100, width=100, onClick, router, selected, setSelected, relation}) => {
+	let buttonStyle = styles.enabled
+	if (selected !== null && label === selected) buttonStyle = styles.active
+	if (selected !== null && label !== selected) buttonStyle = styles.disabled
+	if (onClick !== undefined) {
+		const rel = (onClick.props.relations || []).join(",")
+		if (rel == relation) setSelected(label)
+		return (
+			<Button 
+				onClick={()=>{
+					function_mapper[onClick.name](({...onClick.props, router, selected, label, relation}))
+					setSelected(label)
+				}}
+				sx={buttonStyle}
+			>
+				<div style={{height, minWidth: width, ...buttonStyle}}>
+					<Image
+						// loader={()=>`/birth-defect-drugs${val.icon}`} 
+						src={makeTemplate(icon, {})}
+						height={height}
+						width={width}
+						layout="responsive"
+						objectFit="contain"
+						alt={label}
+					/>
+				</div>
+			</Button>
+		)
+	} else {
+		return (
+			<Button 
+				sx={buttonStyle}
+			>
 				<div style={{height, minWidth: width}}>
 					<Image
 						// loader={()=>`/birth-defect-drugs${val.icon}`} 
@@ -27,6 +90,37 @@ const Header = ({schema, ...rest}) => {
 						alt={label}
 					/>
 				</div>
+			</Button>
+		)
+	}
+}
+
+
+const Header = ({schema, ...rest}) => {
+	const [selected, setSelected] = useState(null)
+	const router = useRouter()
+	const relation = router.query.relation
+
+	useEffect(()=>{
+		if (relation === undefined) setSelected(null)
+	}, [relation])
+
+	if (!schema) schema = default_schema
+	if (schema === undefined || schema.header === undefined) return null
+	return(
+	<Grid container justifyContent={"center"} alignItems={"center"} spacing={2} style={{marginBottom: 20}}>
+		<Grid item xs={12} align="center">
+			<Typography variant="h4">{schema.header.title}</Typography>
+		</Grid>
+		{((schema.header || {}).subheader||[]).map((props)=>(
+			<Grid item key={props.label} style={{marginLeft: 10, marginRight: 10}}>
+				<IconRenderer
+					router={router}
+					setSelected={setSelected}
+					selected={selected}
+					relation={relation}
+					{...props}
+				/>
 			</Grid>
 		))}
 	</Grid>
