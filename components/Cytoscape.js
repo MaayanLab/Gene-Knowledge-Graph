b31883af7105dd641c45ff69315f8bf280298e4f
John Erol Evangelista <je.evangelista04@gmail.com>
2022-10-13 17:05:30 -0400

    added svg support

--- components/Cytoscape.js
+++ components/Cytoscape.js
@@ -1,11 +1,11 @@
 import cytoscape from 'cytoscape'
 import fcose from 'cytoscape-fcose';
 import avsdf from 'cytoscape-avsdf';
-import popper from 'cytoscape-popper';
+import svg from 'cytoscape-svg';
 
+cytoscape.use( svg );
 cytoscape.use(fcose)
 cytoscape.use(avsdf);
-cytoscape.use( popper )
 
 import CytoscapeComponent from 'react-cytoscapejs'
 export default CytoscapeComponent
