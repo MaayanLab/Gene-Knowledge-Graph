ccc529583f30e4f91c0d40fcca4b4adc718931eb
John Erol Evangelista <je.evangelista04@gmail.com>
2022-10-21 15:50:10 -0400

    added node_env

--- utils/neo4j.js
+++ utils/neo4j.js
@@ -1,6 +1,7 @@
 import neo4j from 'neo4j-driver'
 
+const NEO4J_URL = process.env.NODE_ENV === 'development' ? (process.env.NEO4J_DEV_URL || process.env.NEO4J_URL): process.env.NEO4J_URL
 export const neo4jDriver = neo4j.driver(
-  process.env.NEO4J_URL,
+  NEO4J_URL,
   neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
 )
