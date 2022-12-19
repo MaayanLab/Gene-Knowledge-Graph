508fa3960deb07b45052105dabe36efa47017cb8
John Erol Evangelista <je.evangelista04@gmail.com>
2022-12-19 16:02:30 -0500

    fixed error pages

---
+++ pages/500.js
@@ -0,0 +1,24 @@
+import dynamic from "next/dynamic";
+import { fetch_kg_schema } from "../utils/initialize";
+const Typography = dynamic(() => import('@mui/material/Typography'));
+const Grid = dynamic(() => import('@mui/material/Grid'));
+
+
+export default function Error() {
+    return (
+        <Grid container justifyContent={"center"} sx={{height: "100%"}}>
+            <Grid item>
+                <Typography variant="h5">Oops... we have a server error</Typography>
+            </Grid>
+        </Grid>
+    )
+}
+export async function getStaticProps(ctx) {
+    const schema = await fetch_kg_schema()
+  
+    return {
+        props: {
+            schema,
+        }
+      };
+  }
