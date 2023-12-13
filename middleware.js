bfbe72ed41e15ce445c8b86f5ee10187d5c4c4e7
John Erol Evangelista <je.evangelista04@gmail.com>
2023-12-13 10:56:43 -0500

    removed middleware

--- middleware.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function middleware(request) {
-    if (request.nextUrl.pathname === request.nextUrl.pathname.toLocaleLowerCase())
-        return NextResponse.next();
-    return NextResponse.redirect(`${request.nextUrl.origin}${request.nextUrl.pathname.toLocaleLowerCase()}${request.nextUrl.search}`);
-}
