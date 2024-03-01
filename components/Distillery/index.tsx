2d7699842a1375f5d004470aa8508bb84ffb70cd
John Erol Evangelista <je.evangelista04@gmail.com>
2024-03-01 18:10:33 -0500

    updated apps

--- components/Distillery/index.tsx
+++ components/Distillery/index.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { Grid,
 	Typography,
 	Card,
@@ -27,22 +28,18 @@ export default function DistilleryLanding({
 	}>
 }) {
     return  (
-        <Grid container spacing={2} alignItems={"center"}>
+        <Grid container spacing={2} alignItems={"flex-start"}>
             <Grid item xs={12}>
                 <Typography variant={"h2"}>{title}</Typography>
                 <Typography variant={"subtitle1"}>{description}</Typography>
             </Grid>
             {pages.map((page)=>(
                  <Grid item key={page.props.title}>
-                    <Card sx={{ maxWidth: 345, height: 300 }}>
+                    <Card sx={{ maxWidth: 345, minHeight: 300 }}>
                         <CardHeader
-                            avatar={
-                            page.props.icon ?
-                            <Avatar src={page.props.icon} alt={page.props.title}/>:
-                            <Avatar>
+                            avatar={<Avatar>
                                 {page.props.title[0]}
-                            </Avatar>
-                            }
+                            </Avatar>}
                             action={
                                 <Link href={page.endpoint}>
                                     <IconButton aria-label="settings">
@@ -52,7 +49,26 @@ export default function DistilleryLanding({
                             }
                             title={page.props.title}
                         />
+                        {page.props.icon &&
+                                <div className="flex flex-row justify-center">
+                                    <Image src={page.props.icon} alt={page.props.title} height={100} width={100}/>
+                                </div>
+                                // <div className="flex flex-row justify-center"
+                                //     style={{
+                                //     background: "linear-gradient(diagonal, #336699, #006666)",
+                                //     overflow: "hidden",  
+                                //     // height: "100%",
+                                //     minHeight: 70,
+                                //     position: "relative",
+                                //     zIndex: 2
+                                //     }}
+                                // >
+                                //     <Image src={page.props.icon} alt={page.props.title} fill={true} style={{objectFit: "contain"}}/>
+                                //     {/* <Image src={e.image} alt={e.title} width={400} height={300}/> */}
+                                // </div>
+                            }
                         <CardContent>
+                            
                             <Typography variant="body1">
                                 {page.props.description}
                             </Typography>
