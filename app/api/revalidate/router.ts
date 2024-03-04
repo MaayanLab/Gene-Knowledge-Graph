c41e271f0e08b7c6d8790f839eaccf6da48e2d95
John Erol Evangelista <je.evangelista04@gmail.com>
2024-03-04 18:49:53 -0500

    added revalidate

---
+++ app/api/revalidate/router.ts
@@ -0,0 +1,18 @@
+// https://nextjs.org/docs/app/api-reference/functions/revalidatePath
+import { revalidatePath } from 'next/cache'
+import { NextRequest } from 'next/server'
+ 
+export async function GET(request: NextRequest) {
+  const path = request.nextUrl.searchParams.get('path')
+ 
+  if (path) {
+    revalidatePath(path)
+    return Response.json({ revalidated: true, now: Date.now() })
+  }
+ 
+  return Response.json({
+    revalidated: false,
+    now: Date.now(),
+    message: 'Missing path to revalidate',
+  })
+}
