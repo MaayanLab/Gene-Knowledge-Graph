25947bb9de2be8d5ad915fef2b8683693bf9c7d6
John Erol Evangelista <je.evangelista04@gmail.com>
2024-03-04 19:53:57 -0500

    fixed router issue

--- app/api/revalidate/router.ts
+++ app/api/revalidate/router.ts
@@ -1,16 +1,16 @@
 // https://nextjs.org/docs/app/api-reference/functions/revalidatePath
 import { revalidatePath } from 'next/cache'
-import { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
  
 export async function GET(request: NextRequest) {
   const path = request.nextUrl.searchParams.get('path')
  
   if (path) {
     revalidatePath(path)
-    return Response.json({ revalidated: true, now: Date.now() })
+    return NextResponse.json({ revalidated: true, now: Date.now() })
   }
  
-  return Response.json({
+  return NextResponse.json({
     revalidated: false,
     now: Date.now(),
     message: 'Missing path to revalidate',
