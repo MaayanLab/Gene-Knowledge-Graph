8bd2ae42c6109e15c90e4b2e44296d96579d24f7
John Erol Evangelista <je.evangelista04@gmail.com>
2024-03-12 20:30:20 -0400

    fixed deployment

--- cli/version-compose.ts
+++ cli/version-compose.ts
@@ -4,4 +4,4 @@ import path from 'path'
 let compose = fs.readFileSync(path.join(__dirname, '..', 'docker-compose-template.yml'), { encoding: 'utf-8' })
 compose = compose.replaceAll(/\$\{APP_NAME\}/g, process.env.APP_NAME)
 compose = compose.replaceAll(/\$\{VERSION\}/g, process.env.npm_package_version)
-fs.writeFileSync(path.join(__dirname, '..', `docker-compose.${process.env.APP_NAME}.yml`), compose)
+fs.writeFileSync(path.join(__dirname, '..', `docker-compose.yml`), compose)
