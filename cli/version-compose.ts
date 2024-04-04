ac64be9d25c66393979197047a3595495f2c3031
John Erol Evangelista <je.evangelista04@gmail.com>
2024-04-04 13:27:34 -0400

    updated template

--- cli/version-compose.ts
+++ cli/version-compose.ts
@@ -10,4 +10,5 @@ let compose = fs.readFileSync(path.join(__dirname, '..', filename), { encoding: 
 compose = compose.replaceAll(/\$\{APP_NAME\}/g, process.env.APP_NAME)
 compose = compose.replaceAll(/\$\{VERSION\}/g, process.env.npm_package_version)
 compose = compose.replaceAll(/\$\{NEXT_PUBLIC_HOST\}/g, process.env.NEXT_PUBLIC_HOST)
+compose = compose.replaceAll(/\$\{DOCKERHUB_NAME\}/g, process.env.DOCKERHUB_NAME)
 fs.writeFileSync(path.join(__dirname, '..', `docker-compose.yml`), compose)
