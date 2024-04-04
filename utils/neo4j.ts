966ec09a8232b9fe898244afab2e218a84fe6e43
John Erol Evangelista <je.evangelista04@gmail.com>
2024-04-04 19:55:33 -0400

    fixed env

--- utils/neo4j.ts
+++ utils/neo4j.ts
@@ -1,7 +1,7 @@
 import neo4j from 'neo4j-driver'
 
 const neo4jDriverFunc = () => {
-  const NEO4J_URL = process.env.NEXT_PUBLIC_NEO4J_URL
+  const NEO4J_URL = process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_NEO4J_DEV_URL: process.env.NEXT_PUBLIC_NEO4J_URL
   return neo4j.driver(
     NEO4J_URL,
     neo4j.auth.basic(process.env.NEXT_PUBLIC_NEO4J_USER, process.env.NEXT_PUBLIC_NEO4J_PASSWORD)
