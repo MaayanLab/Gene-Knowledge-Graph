eaec3fe3f8e8e4050b5bda2c400f5f855b6c2048
John Erol Evangelista <je.evangelista04@gmail.com>
2024-06-05 12:30:58 -0400

    auto prod url v5

--- utils/neo4j.ts
+++ utils/neo4j.ts
@@ -1,7 +1,8 @@
 import neo4j from 'neo4j-driver'
 
 const neo4jDriverFunc = () => {
-  const NEO4J_URL = process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_NEO4J_DEV_URL: process.env.NEXT_PUBLIC_NEO4J_URL
+  const prod_url = process.env.NEO4J_VERSION === '5' ? process.env.NEXT_PUBLIC_NEO4J_V5_URL: process.env.NEXT_PUBLIC_NEO4J_URL
+  const NEO4J_URL = process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_NEO4J_DEV_URL: prod_url
   return neo4j.driver(
     NEO4J_URL,
     neo4j.auth.basic(process.env.NEXT_PUBLIC_NEO4J_USER, process.env.NEXT_PUBLIC_NEO4J_PASSWORD)
