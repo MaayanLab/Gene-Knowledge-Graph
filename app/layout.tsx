352e6ef3177c6324a8c12706846481683a41d6b9
John Erol Evangelista <je.evangelista04@gmail.com>
2024-06-06 12:45:42 -0400

    fixed layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,10 @@
-import { Metadata, ResolvingMetadata } from 'next'
+import { Metadata } from 'next'
 import ThemeRegistry from './ThemeRegistry'
 import './global.css'
 import { fetch_kg_schema } from '@/utils/initialize'
 
  
-export async function generateMetadata(
-  { params, searchParams }: {
-    params: { id: string }
-    searchParams: { [key: string]: any }
-  },
-  parent: ResolvingMetadata
-): Promise<Metadata> {
-  // read route params
-  const id = params.id
+export async function generateMetadata(): Promise<Metadata> {
  
   // fetch data
   const {header} = await fetch_kg_schema()
