21857ca3b5d0678f16dbec89152b18e23f69b794
Nia Lingam <nlingam@andrew.cmu.edu>
2024-07-31 16:51:25 -0400

    deleted Gene

--- utils/neo4j.ts
+++ utils/neo4j.ts
@@ -3,7 +3,6 @@ import neo4j from 'neo4j-driver'
 const neo4jDriverFunc = () => {
   const prod_url = process.env.NEO4J_VERSION === '5' ? process.env.NEXT_PUBLIC_NEO4J_V5_URL: process.env.NEXT_PUBLIC_NEO4J_URL
   const NEO4J_URL = process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_NEO4J_DEV_URL: prod_url
-  console.log(process.env.NEXT_PUBLIC_NEO4J_USER, process.env.NEXT_PUBLIC_NEO4J_PASSWORD)
   return neo4j.driver(
     NEO4J_URL,
     neo4j.auth.basic(process.env.NEXT_PUBLIC_NEO4J_USER, process.env.NEXT_PUBLIC_NEO4J_PASSWORD)
