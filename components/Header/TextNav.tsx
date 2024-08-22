22a96af744f77df655d66433bb1c6eb0018b2ac2
John Erol Evangelista <je.evangelista04@gmail.com>
2024-08-22 18:23:48 -0400

    added small nav

--- components/Header/TextNav.tsx
+++ components/Header/TextNav.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useState } from "react"
 import Link from "next/link"
-import { Typography, Menu, MenuItem, ButtonBase } from "@mui/material"
+import { Typography, Menu, MenuItem, ButtonBase, List, ListItem, Collapse } from "@mui/material"
 import { usePathname } from "next/navigation"
 
 export  function TextNav({title, path, props}: {
@@ -16,6 +16,7 @@ export  function TextNav({title, path, props}: {
 }) {
 	const pathname = `/${usePathname().split("/")[1]}`
 	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+	const [clicked, setClicked] = useState<null | string>(null);
   	const open = Boolean(anchorEl);
 	let sx
 	if (pathname === path) {
@@ -24,7 +25,8 @@ export  function TextNav({title, path, props}: {
 	}
 	if (props !== undefined && props.submenu !== undefined) {
 		const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-			setAnchorEl(event.currentTarget);
+			if (anchorEl) setAnchorEl(null)
+			else setAnchorEl(event.currentTarget);
 		};
 		const handleClose = () => {
 			setAnchorEl(null);
@@ -37,7 +39,7 @@ export  function TextNav({title, path, props}: {
 					aria-haspopup="true"
 					aria-expanded={open ? 'true' : undefined}
 					onClick={handleClick}
-					sx={{padding: 0, marginLeft: 0}}
+					sx={{padding: 0, marginLeft: 0, display: {xs: "none", sm: "none", md: "block"}}}
 				>
 					<Typography variant="nav" sx={sx}>{title}</Typography>
 				</ButtonBase>
@@ -52,7 +54,8 @@ export  function TextNav({title, path, props}: {
 					sx={{
 						"& .MuiMenu-paper": {
 							backgroundColor: "primary.main"
-						}
+						},
+						display: {xs: "none", sm: "none", md: "block"}
 					}}
 				>
 					{props.submenu.map(({name, href})=>(
@@ -63,13 +66,43 @@ export  function TextNav({title, path, props}: {
 						</MenuItem>
 					))}
 				</Menu>
+				<ListItem sx={{display: {xs: "block", sm: "block", md: "none", lg: "none", xl: "none"}}}>
+					<ButtonBase
+						id="basic-button"
+						aria-controls={open ? 'basic-menu' : undefined}
+						aria-haspopup="true"
+						aria-expanded={open ? 'true' : undefined}
+						onClick={()=>clicked===title?setClicked(null): setClicked(title)}
+						sx={{padding: 0, marginLeft: 0}}
+					>
+						<Typography variant="nav" sx={sx}>{title}</Typography>
+					</ButtonBase>
+				</ListItem>
+				<Collapse in={clicked===title} timeout="auto" unmountOnExit  sx={{display: {xs: "block", sm: "block", md: "none", lg: "none", xl: "none"}}}>
+					<List component="div" disablePadding>
+						{props.submenu.map(({name, href})=>(
+							<ListItem key={name} onClick={handleClose}>
+								<Link href={href}>
+									<Typography variant="nav">{name}</Typography>
+								</Link>
+							</ListItem>
+						))}
+					</List>
+				</Collapse>
 			</>
 		)
 	} else {
 		return(
-			<Link href={path}>
-				<Typography variant="nav" sx={sx}>{title}</Typography>
-			</Link>
+			<>
+				<Link href={path}>
+					<Typography sx={{display: {xs: "none", sm: "none", md: "block"}, ...sx}} variant="nav">{title}</Typography>
+				</Link>
+				<ListItem sx={{display: {xs: "block", sm: "block", md: "none", lg: "none", xl: "none"}}}>
+					<Link href={path}>
+						<Typography variant="nav">{title}</Typography>
+					</Link>
+				</ListItem>
+			</>
 		)
 	} 
 }
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,7 +1,5 @@
-import Link from 'next/link';
 import {
 	Grid, 
-	Typography, 
 	Stack, 
 	AppBar, 
 	Toolbar,
@@ -11,7 +9,8 @@ import { UISchema } from '@/app/api/schema/route';
 import { Logo } from '../misc/logo';
 import Counter from '../Counter';
 import { TextNav } from './TextNav';
-import { fetch_kg_schema } from '@/utils/initialize';
+import SmallNav from "./SmallNav";
+
 export const Nav = ({tabs, ui_theme, divider, title, icon, counterTop, counter}:
 	{
 		ui_theme?: string,
@@ -44,7 +43,7 @@ export const Nav = ({tabs, ui_theme, divider, title, icon, counterTop, counter}:
 		tab_component[position].push(
 			<TextNav key={tab.label} path={tab.endpoint} title={tab.label} props={props}/>
 		)
-		if (divider) tab_component[position].push(<Divider orientation='vertical' flexItem sx={{borderColor: "#000"}}/>)
+		if (divider) tab_component[position].push(<Divider key={tab.label + "div"} sx={{display: {xs: "none", sm: "none", md: "block", borderColor: "#000"}}} orientation='vertical' flexItem/>)
 	}
 	if (divider) {
 		for (const position of Object.keys(tab_component)) {
@@ -56,7 +55,7 @@ export const Nav = ({tabs, ui_theme, divider, title, icon, counterTop, counter}:
 			<Grid item sx={{ flexGrow: 1 }}>
 				<Logo alt={icon.alt} src={icon.favicon} title={title} avatar={icon.avatar} width={icon.width} height={icon.height} size='small' color="secondary"/>
 			</Grid>
-			<Grid item>
+			<Grid item sx={{display: {xs: "none", sm: "none", md: "block"}}}>
 				<Stack direction={"row"} alignItems={"center"} spacing={2}>
 					{tab_component.top}
 					{(counter && tab_component.bottom.length === 0 && counterTop) && 
@@ -65,7 +64,7 @@ export const Nav = ({tabs, ui_theme, divider, title, icon, counterTop, counter}:
 				</Stack>
 			</Grid>
 			{tab_component.bottom.length > 0 &&
-				<Grid item xs={12}>
+				<Grid item xs={12} sx={{display: {xs: "none", sm: "none", md: "block"}}}>
 					<Stack direction={"row"} justifyContent={"space-between"} alignItems={"center"} spacing={2}>
 						<Stack direction={"row"} alignItems={"center"} spacing={2}>
 							{tab_component.bottom}		
@@ -76,10 +75,13 @@ export const Nav = ({tabs, ui_theme, divider, title, icon, counterTop, counter}:
 				</Grid>
 			}
 			{(counter && tab_component.bottom.length === 0 && !counterTop) &&
-				<Grid item xs={12} className='flex justify-end'>		
+				<Grid item xs={12} className='flex justify-end' sx={{display: {xs: "none", sm: "none", md: "block"}}}>		
 					<Counter ui_theme={ui_theme}/>
 				</Grid>
 			}
+			<Grid item className='flex justify-end' sx={{display: {xs: "block", sm: "block", md: "none", lg: "none", xl: "none"}}}>
+				<SmallNav counter={counter} tab_component={tab_component} ui_theme={ui_theme}/>
+			</Grid>
 		</Grid>
 	)
 }
