2ea7ff125b0cecb6df7ef40a759c60cf3c3eb6b8
John Erol Evangelista <je.evangelista04@gmail.com>
2024-09-04 13:00:55 -0400

    mod api

--- app/api/knowledge_graph/all/route.ts
+++ app/api/knowledge_graph/all/route.ts
@@ -7,25 +7,16 @@ import { ArrowShape } from "@/components/Cytoscape";
 async function process_query({
     aggr_scores,
     colors,
-    field,
     arrow_shape}: {
         aggr_scores?: {[key:string]: {max: number, min: number}},
         colors?: {[key: string]: {color?: string, field?: string, aggr_type?: string}},
-        field: string,
         arrow_shape?: {[key:string]: ArrowShape},
     }) {
     const query = `MATCH p=()-[]-()
                     RETURN p, nodes(p) as n, relationships(p) as r
     `
-    return resolve_results({query, aggr_scores, colors, fields: [field], arrow_shape})
+    return resolve_results({query, aggr_scores, colors, fields: [], arrow_shape})
 }
-
-const InputSchema = z.object({
-    start: z.string(),
-    start_term: z.string(),
-    limit: z.number().optional(),
-    start_field: z.string().optional(),
-})
 /**
  * @swagger
  * /api/distillery/disease2exrna:
@@ -80,21 +71,15 @@ const InputSchema = z.object({
 export async function GET(req: NextRequest) {
     try {
         const filter = req.nextUrl.searchParams.get("filter")
-        if (!filter) return NextResponse.json({error: "No filter inputted"}, {status: 400})
-        const f = JSON.parse(filter)
-        if (f.limit && !isNaN(f.limit) && typeof f.limit === 'string') f.limit = parseInt(f.limit)
-        const { start, start_term, limit=10, start_field="label" } = InputSchema.parse(f)
+        
         
         const {aggr_scores, colors, arrow_shape} = await initialize()
         
-        if (start_term === undefined) return NextResponse.json({error: "No term inputted"}, {status: 400})
-        else { 
-            try {
-                const results = await process_query({ aggr_scores, colors, field:start_field, arrow_shape })
-                return NextResponse.json(results, {status: 200})
-            } catch (e) {
-                return NextResponse.json(e, {status: 400})
-            }
+        try {
+            const results = await process_query({ aggr_scores, colors, arrow_shape })
+            return NextResponse.json(results, {status: 200})
+        } catch (e) {
+            return NextResponse.json(e, {status: 400})
         }
     } catch (e) {
         return NextResponse.json(e, {status: 400})
