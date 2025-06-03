b1941e5e1b525c24f861b2360e8fdb634b7c6130
John Erol Evangelista <je.evangelista04@gmail.com>
2025-06-03 13:16:11 -0400

    fixed card height

--- components/Distillery/index.tsx
+++ components/Distillery/index.tsx
@@ -28,14 +28,14 @@ export default function DistilleryLanding({
 	}>
 }) {
     return  (
-        <Grid container spacing={2} alignItems={"flex-start"}>
+        <Grid container spacing={2}>
             <Grid item xs={12}>
                 <Typography variant={"h2"}>{title}</Typography>
                 <Typography variant={"subtitle1"}>{description}</Typography>
             </Grid>
             {pages.map((page)=>(
-                 <Grid item key={page.props.title}>
-                    <Card sx={{ maxWidth: 345, minHeight: 320 }}>
+                 <Grid item xs={6} md={4} key={page.props.title}>
+                    <Card sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
                         <CardHeader
                             avatar={<Avatar>
                                 {page.props.title[0]}
@@ -50,12 +50,19 @@ export default function DistilleryLanding({
                             title={page.props.title}
                         />
                         {page.props.icon &&
-                                <div className="flex flex-row justify-center">
+                                <div className="flex flex-row justify-center"
+                                    style={{
+                                        overflow: "hidden",  
+                                        minHeight: 100,
+                                        position: "relative",
+                                        zIndex: 2, 
+                                        }}
+                                >
                                     <Image src={page.props.icon} alt={page.props.title} height={page.props.height || 100} width={page.props.width || 100}/>
                                 </div>
                             }
-                        <CardContent>
-                            
+                        <CardContent  sx={{flexGrow: 2, height: "100%", justifyContent: "flex-end"}}>
+                          
                             <Typography variant="body1">
                                 {page.props.description}
                             </Typography>
