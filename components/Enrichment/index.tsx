322118f686b7e8659c6f2e5c7b667362e0dec5e5
John Erol Evangelista <je.evangelista04@gmail.com>
2025-07-17 15:23:49 -0400

    fixed turl

--- components/Enrichment/index.tsx
+++ components/Enrichment/index.tsx
@@ -173,10 +173,10 @@ const Enrichment = async ({
         }
         const payload = {
             'url': `${process.env.NODE_ENV==="development" ? process.env.NEXT_PUBLIC_HOST_DEV : process.env.NEXT_PUBLIC_HOST}${process.env.NEXT_PUBLIC_PREFIX ? process.env.NEXT_PUBLIC_PREFIX: ''}${endpoint}?q=${searchParams.q}`,
-            'apikey': process.env.NEXT_PUBLIC_TURL  
+            'apikey': process.env.TURL  
         }
         console.log("Getting short url")
-        const request = await fetch(process.env.NEXT_PUBLIC_TURL_URL, {
+        const request = await fetch(process.env.TURL_URL, {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json",
