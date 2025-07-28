8e1248dfc74e647741aef14f4759c5e2d432cb79
John Erol Evangelista <je.evangelista04@gmail.com>
2025-07-28 13:37:13 -0400

    updated bkg

--- components/Counter.tsx
+++ components/Counter.tsx
@@ -5,7 +5,6 @@ const fetcher = (url) => fetch(url).then(r => r.json())
 export const Counter = ({ui_theme}: {ui_theme?: string}) => {  
 
     const { data } = useSWR(`${process.env.NODE_ENV==="development" ? process.env.NEXT_PUBLIC_HOST_DEV : process.env.NEXT_PUBLIC_HOST}${process.env.NEXT_PUBLIC_PREFIX ? process.env.NEXT_PUBLIC_PREFIX: ''}/api/counter`, fetcher, { refreshInterval: 5000 })
-
     return (
         <Stack direction={"row"} spacing={1}>
             <Typography variant="nav">Queries Submitted: </Typography> 
