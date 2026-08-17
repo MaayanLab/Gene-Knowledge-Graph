829235f3082ad9ec4b23a16dfa09c4fc23ebd507
John Erol Evangelista <je.evangelista04@gmail.com>
2026-08-17 17:08:57 -0400

    UPDATED BOLT

--- utils/neo4j.ts
+++ utils/neo4j.ts
@@ -1,9 +1,8 @@
 import neo4j from 'neo4j-driver'
 
 const neo4jDriverFunc = () => {
-  const prod_url = process.env.NEO4J_VERSION === '5' ? process.env.NEO4J_V5_URL: process.env.NEO4J_URL
-  const NEO4J_URL = process.env.NODE_ENV === 'development' ? process.env.NEO4J_DEV_URL: prod_url
-  console.log(prod_url)
+  const NEO4J_URL = process.env.NODE_ENV === 'development' ? process.env.NEO4J_DEV_URL: process.env.NEO4J_URL
+  console.log(NEO4J_URL)
   return neo4j.driver(
     NEO4J_URL,
     neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
