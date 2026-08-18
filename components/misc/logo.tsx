f4b5965e5fc992c02706440720848356905dae69
John Erol Evangelista <je.evangelista04@gmail.com>
2026-08-18 14:38:53 -0400

    fixed links

--- components/misc/logo.tsx
+++ components/misc/logo.tsx
@@ -21,7 +21,7 @@ export const Logo = ({src, alt, title, color="secondary", size, avatar}: {src: s
             }
             <div>
                 <Typography variant={size==='large'?'cfde':'cfde_small'} color={color} dangerouslySetInnerHTML={sanitize(title)}></Typography>
-                <Image src="/img/enrichr-kg.jpg" alt="enrichr-kg-header" width={200} height={100}/>
+                <Image src={`${process.env.NEXT_PUBLIC_PREFIX}/img/enrichr-kg.jpg`} alt="enrichr-kg-header" width={200} height={100}/>
             </div>
         </Link>
     )
