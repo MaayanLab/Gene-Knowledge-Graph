f1428ef3133ea26b8cf3a0721ade7b5125714597
John Erol Evangelista <je.evangelista04@gmail.com>
2026-08-18 19:30:57 -0400

    modified args

--- components/Counter.tsx
+++ components/Counter.tsx
@@ -4,7 +4,7 @@ import useSWR from 'swr';
 const fetcher = (url) => fetch(url).then(r => r.json())
 export const Counter = ({ui_theme}: {ui_theme?: string}) => {  
 
-    const { data } = useSWR(`${process.env.NODE_ENV==="development" ? process.env.NEXT_PUBLIC_HOST_DEV : process.env.NEXT_PUBLIC_HOST}${process.env.NEXT_PUBLIC_PREFIX ? process.env.NEXT_PUBLIC_PREFIX: ''}/api/counter`, fetcher, { refreshInterval: 5000 })
+    const { data } = useSWR(`${process.env.NEXT_PUBLIC_PREFIX ? process.env.NEXT_PUBLIC_PREFIX: ''}/api/counter`, fetcher, { refreshInterval: 5000 })
 
     return (
         <Stack direction={"row"} spacing={1}>
